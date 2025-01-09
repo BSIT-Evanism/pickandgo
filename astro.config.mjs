5f372cb001be621bdeda5af55c84913d0681ba57
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2025-01-10 01:25:53 +0800

    qf2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,13 @@ import vercel from '@astrojs/vercel';
 
 import partytown from '@astrojs/partytown';
 
-import yeskunallumami from '@yeskunall/astro-umami';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    yeskunallumami({
-      id: '7aa7460b-bb4d-4a2f-ae97-6e18ff4f4ca3',
-    }),
+    partytown(),
   ],
   output: 'server',
   adapter: vercel(),
