e7b31990f11abbec5c62a7bf40152893b14db7cc
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2025-01-23 14:45:02 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,37 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
-import swup from '@swup/astro';
-import node from '@astrojs/node';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import swup from "@swup/astro";
+import node from "@astrojs/node";
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   env: {
     schema: {
       DATABASE_URL: envField.string({
-        context: 'server',
-        access: 'secret',
-        optional: false
+        context: "server",
+        access: "secret",
+        optional: false,
       }),
       BETTER_AUTH_SECRET: envField.string({
-        context: 'server',
-        access: 'secret',
-        optional: false
-      })
-    }
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+    },
   },
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
